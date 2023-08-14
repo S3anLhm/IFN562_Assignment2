c10d216053d1006ba92d09f268236aaf08f51010
S3anLhm <136083568+S3anLhm@users.noreply.github.com>
2023-08-14 22:54:34 +1000

    Created Main FIle

---
+++ Game/Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
